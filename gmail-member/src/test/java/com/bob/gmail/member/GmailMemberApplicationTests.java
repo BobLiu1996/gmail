7d1ad00e1c26bd22c6b70fda4a70470b73064255
BobLiu1996 <42339376+BobLiu1996@users.noreply.github.com>
2020-05-12 09:32:37 +0800

    项目初始框架创建

---
+++ gmail-member/src/test/java/com/bob/gmail/member/GmailMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.gmail.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
